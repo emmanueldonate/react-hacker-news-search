05a7b937535c08261809f0bf178c0160e53340a5
Emmanuel <e.o.donate@gmail.com>
2018-12-07 14:45:11 -0800

    Update search response

--- src/App.js
+++ src/App.js
@@ -1,27 +1,70 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 export default function App() {
   const [results, setResults] = useState([]);
+  const [query, setQuery] = useState('react hooks');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const searchInputRef = useRef();
+
   useEffect(() => {
-    axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
-    .then(response => {
-      console.log(response.data);
-      setResults(response.data.hits);
-    });
+    getResults();
   }, []);
+
+  const getResults = async () => {
+    setLoading(true)
+
+    try {
+    const response = await axios.get(
+      `http://hn.algolia.com/api/v1/searchquery=r${query}`
+      );
+    setResults(response.data.hits);
+    } catch (err) {
+        setError(err)
+    }
+    setLoading(false)
+
+  };
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  };
+
+  const handleClearSearch = () => {
+    setQuery("");
+    searchInputRef.current.focus();
+  };
 
   return (
    <>
+   <form onSubmit={handleSearch}>
+   <input 
+    type="text" 
+    onChange={event => setQuery(event.target.value)} 
+    value={query} 
+    ref={searchInputRef}
+    />
+    <button type="submit">Search</button>
+    <button type="button" onClick={handleClearSearch}>Clear</button>
+    </form>
+    {loading ? (
+       <div>Loading results...</div>
+     ) : (
    <ul>
+   
    {results.map(result => (
      <li key={result.objectID}>
      <a href={result.url}>{result.title}</a>
      </li>
    ))}
-   </ul>
-   </>
-  )
+   </ul>)}
 
-}
+   {error && <div>{error.message}</div>}
+
+   </>
+  );
+
+};
 
